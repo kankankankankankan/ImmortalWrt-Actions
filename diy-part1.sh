0fa384591104321ac38dade9da0b87be1bbf5587
kankankankankankan <vip029@me.com>
2026-09-25 14:32:32 +0800

    恢复 diy-part1.sh 为追加官方源的方式

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,5 @@
 set -e
 set -o pipefail
 
-# 添加自定义软件源，插到 feeds.conf.default 最前面（在官方 packages 之前），同名包以咱们的优先
-sed -i '1i src-git immortalwrt https://github.com/kankankankankankan/ImmortalWrt-Packages;main' feeds.conf.default
+# 添加自定义软件源，追加至 feeds.conf.default 文件末尾
+echo 'src-git immortalwrt https://github.com/kankankankankankan/ImmortalWrt-Packages;main' >> feeds.conf.default
