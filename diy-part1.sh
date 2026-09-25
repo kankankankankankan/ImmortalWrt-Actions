62f60b79904196818e8ee7480872bb403d26049c
kankankankankankan <vip029@me.com>
2026-09-25 14:35:12 +0800

    diy-part1: 自定义源置于官方源之前，同名包优先采用

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,5 @@
 set -e
 set -o pipefail
 
-# 添加自定义软件源，追加至 feeds.conf.default 文件末尾
-echo 'src-git immortalwrt https://github.com/kankankankankankan/ImmortalWrt-Packages;main' >> feeds.conf.default
+# 添加自定义软件源，插入至 feeds.conf.default 首行，置于官方软件源之前，以确保同名软件包优先采用本仓库所提供之版本
+sed -i '1i src-git immortalwrt https://github.com/kankankankankankan/ImmortalWrt-Packages;main' feeds.conf.default
